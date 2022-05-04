4c727e608b74afc88893fd71acff653d131da71e
NicolineRusbak <1081543@ucn.dk>
2022-05-04 13:36:57 +0200

    Added normalize css

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import 'normalize.css/normalize.css'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
