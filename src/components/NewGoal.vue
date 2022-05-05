b97971a7b19f4c2438cf1b5bf0351bfc248e553a
KamillaKlavsen <kamillaklavsen@outlook.dk>
2022-05-05 12:45:35 +0200

    GoalsView og NewGoal connected

---
+++ src/components/NewGoal.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>hello goals</h1>
+</template>
--- src/views/GoalsView.vue
+++ src/views/GoalsView.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="todo">
-    <h1>This is an goals page</h1>
+    <h1>This is a goals page</h1>
     <h2>test</h2>
+    <NewGoal />
   </div>
 </template>
+
+<script>
+import NewGoal from '../components/NewGoal.vue';
+
+export default {
+name: 'GoalsView',
+components: { NewGoal },
+ };
+</script>
+
+<style lang='less' scoped>
+
+
+</style>
