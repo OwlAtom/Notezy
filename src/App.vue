4e4a73c11e9f3a119c53ddfb035063b43e32dbf8
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-05-06 11:40:47 +0200

    Import fonts

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,40 @@ export default {
 </script>
 
 <style lang="less">
-
 //main color
-@main-blue:#626ADE;
-@main-white:#FAFAFA;
-@main-dark-color: #3B252C;
+@main-blue: #626ade;
+@main-white: #fafafa;
+@main-dark-color: #3b252c;
 
 //user colors
-@green:#68DEA3;
-@blue:#818EF5;
-@light-blue: #8AEDE5;
-@lavender: #D79EF4;
-@red:#ED84A0;
-@orange:#F7BE87;
-@yellow: #F1DE79;
+@green: #68dea3;
+@blue: #818ef5;
+@light-blue: #8aede5;
+@lavender: #d79ef4;
+@red: #ed84a0;
+@orange: #f7be87;
+@yellow: #f1de79;
+
+// google fonts
+// todo: only load font weights that are used (do this after styling is complete)
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap");
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Bebas Neue", cursive;
+}
+p,
+a,
+button,
+input,
+li,
+label {
+  font-family: "Nunito Sans", sans-serif;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
