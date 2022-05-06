9ba53e6a678e6e47f61e0d2461777267c7ae9bcf
KamillaKlavsen <kamillaklavsen@outlook.dk>
2022-05-06 14:04:06 +0200

    begyndende styling på tracker

--- src/components/NewGoal.vue
+++ src/components/NewGoal.vue
@@ -1,3 +1,44 @@
 <template>
-    <h1>hello goals</h1>
+    <div class="wrapper">
+    <div class="goal">
+        <h4>Goal name</h4>
+        <div class="checkboxes">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        <input type="checkbox" name="checkbox" value="day" id="day">
+        </div>
+    </div>
+    </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.wrapper{
+    display: flex;
+    justify-content: center;
+    margin: 0 .8em;
+}
+.goal{
+    background-color:#68DEA3;
+    height: 7.6em;
+    width: 80%;
+    margin: 1.5em 1.5em;
+    border-radius: .5em;
+}
+input[type=checkbox]{
+    margin: 0.3em 1em;
+    transform: scale(2.5);
+    border: 1px black solid;
+}
+    
+
+</style>
--- src/views/GoalsView.vue
+++ src/views/GoalsView.vue
@@ -2,6 +2,17 @@
   <div class="todo">
     <h1>This is a goals page</h1>
     <h2>test</h2>
+    <div>
+      <ul>
+        <li>M</li>
+        <li>T</li>
+        <li>W</li>
+        <li>T</li>
+        <li>F</li>
+        <li>S</li>
+        <li>S</li>
+      </ul>
+    </div>
     <NewGoal />
   </div>
 </template>
