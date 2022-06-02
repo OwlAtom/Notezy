1fe1f5d9c94abc370de9dcf1c23dc46adb8b057c
KamillaKlavsen <kamillaklavsen@outlook.dk>
2022-06-02 22:24:13 +0200

    toggle switch

--- src/views/GoalsView.vue
+++ src/views/GoalsView.vue
@@ -11,7 +11,14 @@
       <input id="timesPerWeek" type="number" min="1" max="7" value="1" />
 
       <label for="specDays" class="small-title">On specific days</label>
-      <input id="specDays" type="checkbox" v-model="onSpecificDays" />
+      <!-- <input id="specDays" type="checkbox" v-model="onSpecificDays" /> -->
+
+      <!-- test switch  -->
+      <label class="switch">
+        <input id="specDays" type="checkbox" v-model="onSpecificDays" />
+        <span class="slider round"></span>
+      </label>
+
       <div v-show="onSpecificDays" class="day-container smaller-title">
         Choose the days:
         <!-- loop days -->
@@ -294,6 +301,67 @@ router-link {
   position: relative;
   padding: 20px 10px;
 }
+
+// toggle switch
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: var(--main-blue);
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px var(--main-blue);
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+//
 
 .day-container {
   display: flex;
