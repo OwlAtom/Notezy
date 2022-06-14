bde7b78967cd557987f327ff81caa52f468552f5
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-06-14 11:34:45 +0200

    made bool to use/not use emulators

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,20 +1,28 @@
+const useEmulators = true;
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
   apiKey: "AIzaSyARbuR0FhM2OWfKFEMNFykUxNKiisw_jeY",
   authDomain: "ucn-notezy.firebaseapp.com",
-  databaseURL: "http://localhost:9000/?ns=ucn-notezy", // emulator
-  // databaseURL: "https://ucn-notezy-default-rtdb.europe-west1.firebasedatabase.app/", // real
+  databaseURL:
+    "https://ucn-notezy-default-rtdb.europe-west1.firebasedatabase.app/", // real
   projectId: "ucn-notezy",
   storageBucket: "ucn-notezy.appspot.com",
   messagingSenderId: "24839556954",
   appId: "1:24839556954:web:75b20196a6d02507ed2d63",
 };
+
+if (useEmulators) {
+  firebaseConfig.databaseURL = "http://localhost:9000/?ns=ucn-notezy"; // emulator
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -31,6 +39,11 @@ onAuthStateChanged(auth, (user) => {
     console.log("user is not logged in");
   }
 });
+
+if (useEmulators) {
+  const db = getFirestore();
+  connectFirestoreEmulator(db, "localhost", 8880);
+}
 
 // export utils/refs
 export { app, auth };
