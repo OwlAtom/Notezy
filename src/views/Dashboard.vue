44a562a9bb9bdec802b56fe6894171cac4cfecd8
NicolineRusbak <1081543@ucn.dk>
2022-06-15 00:31:31 +0200

    Forsøg på at importere goals til dash

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,12 +7,18 @@
     class="pfp"
   />
   <AddTodoButton />
+  <template v-for="goal in goalStore.goals" :key="goal.id">
+    <!-- Skal kun hente goals med id -->
+    <SingleGoal v-if="(goal.id = 1)" :goal="goal" />
+  </template>
 </template>
 
 <script>
 import { userStore } from "../store/user";
+import { goalStore } from "../store/goals";
 import logoutButton from "@/components/logoutButton.vue";
 import AddTodoButton from "@/components/AddTodoButton.vue";
+import SingleGoal from "../components/SingleGoal.vue";
 
 export default {
   name: "DashboardView",
@@ -20,10 +26,15 @@ export default {
     user() {
       return userStore();
     },
+    // goals store
+    goalStore() {
+      return goalStore();
+    },
   },
   components: {
     logoutButton: logoutButton,
     AddTodoButton,
+    SingleGoal,
   },
 };
 </script>
